7038d1878897f16ce53316afd3f91b7f5927ca2d
jonatha silva <jonatha.sviegas@hotmail.com>
2025-04-11 10:36:15 -0300

    Colocando IMG

--- contact/forms.py
+++ contact/forms.py
@@ -4,6 +4,14 @@ from . import models
 
 class ContactForm(forms.ModelForm):
 
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept' : 'image/*',
+            }
+        )
+    )
+
     first_name = forms.CharField(
         widget=forms.TextInput(
             attrs={
@@ -16,7 +24,7 @@ class ContactForm(forms.ModelForm):
 
     class Meta:
         model = models.Contact
-        fields = 'first_name', 'last_name' , 'phone' , 'email' , 'description' , 'category' ,
+        fields = 'first_name', 'last_name' , 'phone' , 'email' , 'description' , 'category' , 'picture' ,
 
 
     def clean(self):
