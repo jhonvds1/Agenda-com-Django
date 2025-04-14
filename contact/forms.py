34f70d21421d59495f87ffbe917c0f7c392ef7cf
jonatha silva <jonatha.sviegas@hotmail.com>
2025-04-14 18:02:46 -0300

    Finalizado

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,35 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from . import models
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import password_validation
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField(
+        required=True,
+    )
+    class Meta():
+        model = User
+        fields = 'first_name' , 'last_name' , 'email' , 'username' , 'password1' , 'password2' ,
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este email!', code='invalid')
+            )
+        return email
 
 class ContactForm(forms.ModelForm):
 
@@ -9,7 +38,8 @@ class ContactForm(forms.ModelForm):
             attrs={
                 'accept' : 'image/*',
             }
-        )
+        ),
+        required=False
     )
 
     first_name = forms.CharField(
@@ -53,4 +83,95 @@ class ContactForm(forms.ModelForm):
     def clean_firts_name(self):
         first_name = self.cleaned_data.get('first_name')
         return first_name
+
+
+class RegisterUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Required.',
+        error_messages={
+            'min_length' : 'Please, add more than 2 letters.'
+        }
+    )
+    
+    last_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Required.'
+    )
+    
+    pasaword1 = forms.CharField(
+        label='Password',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete':'new-password'}),
+        help_text=password_validation.password_validators_help_text_html(),
+        required=False
+    )
+    
+    
+    password2 = forms.CharField(
+        label="Password 2",
+        strip=False,
+        widget = forms.PasswordInput(attrs={"autocomplete":"new-passoword"}),
+        help_text = 'Use the same password as before,',
+        required=False,
+    )
+    class Meta():
+        model = User
+        fields = 'first_name' , 'last_name' , 'email' , 'username'
+
+
+    def save(self , commit = True):
+        cleaned_data = self.cleaned_data
+        user = super().save(commit=False)
+        password = cleaned_data.get('password1')
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
+
+
+    def clean(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 or password2:
+            if password1!=password2:
+                self.add_error(
+                    'password2',
+                    ValidationError('As senhas devem ser iguais')
+                )
+
+        return super().clean()
+
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        current_email = self.instance.email
+        if current_email != email:
+            if User.objects.filter(email=email).exists():
+                self.add_error(
+                    'email',
+                    ValidationError('Já existe este email!', code='invalid')
+                )
+        return email
+    
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        if password1:
+            try:
+                password_validation.validate_password(password1)
+            except ValidationError as errors:
+                self.add_error(
+                    'password1',
+                    ValidationError(errors)
+                )
+
+
+        return password1
+
+
 
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,19 +2,22 @@ from django.shortcuts import render, redirect, get_object_or_404
 from contact.forms import ContactForm
 from django.urls import reverse
 from contact.models import Contact
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST , request.FILES)
         context = {
             'form' : form ,
             'form_action' : form_action ,  
         }
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update' , contact_id=contact.id)
 
         return render(
@@ -35,12 +38,12 @@ def create(request):
         context
     )
 
-
+@login_required(login_url='contact:login')
 def update(request , contact_id):
-    contact = get_object_or_404(Contact , id=contact_id , show=True)
+    contact = get_object_or_404(Contact , id=contact_id , show=True, owner=request.user)
     form_action = reverse('contact:update',args=(contact_id,))
     if request.method == 'POST':
-        form = ContactForm(request.POST , instance=contact)
+        form = ContactForm(request.POST ,request.FILES , instance=contact)
         context = {
             'form' : form ,
             'form_action' : form_action ,  
@@ -68,9 +71,9 @@ def update(request , contact_id):
         context
     )
 
-
+@login_required(login_url='contact:login')
 def delete(request , contact_id):
-    contact = get_object_or_404(Contact , id=contact_id , show=True)
+    contact = get_object_or_404(Contact , id=contact_id , show=True, owner=request.user)
     confirmation = request.POST.get('confirmation' , 'no')
     
     if confirmation == 'yes':
